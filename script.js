546389be51e3596a69d494b9dcc7de6165514272
Nox1ee <108680879+Nox1ee@users.noreply.github.com>
2025-11-17 18:10:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+import Game from './jsClass/game.js'
+// находим элемент по селектору
+const canvas = document.querySelector('canvas')
+
+const game = new Game(canvas)
+game.loadAssets().then(() => game.start())
