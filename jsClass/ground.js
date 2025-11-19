b857244355773f0630b911b7a9d6d02ed3154512
Nox1ee <108680879+Nox1ee@users.noreply.github.com>
2025-11-19 20:30:25 +0500

    Update ground.js

--- jsClass/ground.js
+++ jsClass/ground.js
@@ -9,7 +9,6 @@ class Ground {
 		await loadImage(Ground.groundImg, './assets/sprites/sprite.png')
 	}
 
- 
 	constructor(canvas) {
 		this.config = new Config();
 
@@ -20,44 +19,42 @@ class Ground {
 		this.y = this.canvas.height - this.config.ground.height
 	}
 
-	draw(index) {
+	draw() {
+        this.ctx.drawImage(
+            Ground.groundImg,
 
-		this.backgroundX = -((index * this.config.SPEED) % this.canvas.width);
-
-		this.ctx.drawImage(
-			Ground.groundImg,
-
-			276,
-			0,
-			225,
+            276,
+			0, 
+			225, 
 			this.config.ground.height,
 
-			this.backgroundX + this.canvas.width,
+            this.x, 
 			this.canvas.height - this.config.ground.height,
-			this.canvas.width,
+            this.canvas.width + 5, 
 			this.config.ground.height
-		);
+        );
+        
+        this.ctx.drawImage(
+            Ground.groundImg,
 
-		this.ctx.drawImage(
-			Ground.groundImg,
-
-			276,
-			0,
-			225,
+            276, 
+			0, 
+			225, 
 			this.config.ground.height,
 
-			this.backgroundX,
+            this.x + this.canvas.width, 
 			this.canvas.height - this.config.ground.height,
-			this.canvas.width + 5,
+            this.canvas.width, 
 			this.config.ground.height
-		);
+        );
+    }
 
-	}
-
-	update(index) {
-		this.x -= this.config.SPEED
-		this.draw(index)
-	}
+	update(deltaFactor) {
+        this.x -= this.config.SPEED * deltaFactor;
+        if (this.x <= -this.canvas.width) {
+            this.x = 0;
+        }
+    }
 }
 
 export default Ground
