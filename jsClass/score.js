8f975166c28bca274c8a06ed0c2c5e1fef6a05c1
Nox1ee <108680879+Nox1ee@users.noreply.github.com>
2025-11-20 13:41:11 +0500

    Update score.js

--- jsClass/score.js
+++ jsClass/score.js
@@ -18,8 +18,8 @@ class Score {
 	increase() {
 		this.score++;
 		this.config.SPEED += 0.01
-		this.config.SCORE_S.play();
-		this.config.SCORE_S.currentTime = 0;
+		// this.config.SCORE_S.play();
+		// this.config.SCORE_S.currentTime = 0;
 	}
 
 	// Проверка рекорда
@@ -66,3 +66,4 @@ class Score {
 }
 
 export default Score;
+
